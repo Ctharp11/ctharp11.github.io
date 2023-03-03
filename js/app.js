c84d1e88b0bcbec19d65fae96c1cc30d08110bc1
Cameron Tharp <ctharp1988@gmail.com>
2023-03-03 15:37:30 -0600

    service worker, notifications, caching for offline

---
+++ js/app.js
@@ -0,0 +1,35 @@
+if ("serviceWorker" in navigator) {
+navigator.serviceWorker.register("/sw.js");
+}
+
+const notificationButton = document.getElementById("notifications");
+
+if (notificationButton) {
+notificationButton.addEventListener("click", () => {
+    Notification.requestPermission().then((result) => {
+    if (result === "granted") {
+        randomNotification();
+    }
+    });
+});
+}
+
+const getNewQuote = async () => {
+var url = "https://type.fit/api/quotes";
+const response = await fetch(url);
+const allQuotes = await response.json();
+const indx = Math.floor(Math.random() * allQuotes.length);
+return allQuotes[indx];
+};
+
+async function randomNotification() {
+const quote = await getNewQuote();
+const notifTitle = "This is a Quote";
+const notifBody = quote
+    ? `${quote.text} - ${quote.author}`
+    : "There was an error getting the quote.";
+const options = {
+    body: notifBody,
+};
+new Notification(notifTitle, options); // not working on chrome. test in safari
+}
---
+++ sw.js
@@ -0,0 +1,57 @@
+const cacheName = "Cameron Tharp Portfolio-v1";
+
+const contentToCache = [
+  "/",
+  "index.html",
+  "css/style.css",
+  "js/app.js",
+  "js/script.js",
+  "img/icon-512.png",
+  "img/tharp_resume.pdf",
+  "https://type.fit/api/quotes",
+];
+
+self.addEventListener("install", (e) => {
+  console.log("[Service Worker] Install");
+  e.waitUntil(
+    (async () => {
+      const cache = await caches.open(cacheName);
+      console.log("[Service Worker] Caching all: app shell and content");
+      await cache.addAll(contentToCache);
+    })()
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    (async () => {
+      console.log("e.request", e.request);
+      const r = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (r) {
+        return r;
+      }
+      const response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })()
+  );
+});
+
+//clear out old cache
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key === cacheName) {
+            return;
+          }
+          return caches.delete(key);
+        })
+      );
+    })
+  );
+});
