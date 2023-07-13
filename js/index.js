f1f67fe2a20f7689418ef0a62c81c59e02e82fba
Cameron Tharp <ctharp1988@gmail.com>
2023-07-13 12:17:47 -0500

    updated resume and skills

---
+++ js/index.js
@@ -0,0 +1,3 @@
+import * as N from "./notification";
+import * as P from "./playground";
+import * as T from "./textTransition";
--- js/playground.js
+++ js/playground.js
@@ -3,3 +3,9 @@ pwa.addEventListener("click", () => {
   const url = "https://github.com/Ctharp11/pwa-template";
   window.open(url, "_blank").focus();
 });
+
+const jsUtilities = document.getElementById("js-utilities");
+jsUtilities.addEventListener("click", () => {
+  const url = "https://github.com/Ctharp11/js-utilities";
+  window.open(url, "_blank").focus();
+});
